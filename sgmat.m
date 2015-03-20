12a2344038926c978a5197bda52c3817a51ba2e5
Matthew Dutton <matt.dutton@motionreality.com>
2015-03-20 11:51:33 -0400

    Document sgmat.m

--- sgmat.m
+++ sgmat.m
@@ -1,7 +1,9 @@
-%Ac=y
-%c=inv(A'*A)*A'*y
-%c=ret*y
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This produces a Savitsky-Golay convultion matrix such that the coefficients
+% of the fit polynomial are (e.g.):
+% trans([x v a]) = sgmat(nl,nr,2) * trans( [y_{-nl} ... y_0 ... y_{nr}] )
 function ret = sgmat( nl, nr, order )
+    %printf( "sgmat( %u, %u, %u ):\n", nl, nr, order );
     A = fliplr( vander(-nl:nr, order+1) );
     ret = inv( A'*A ) * A';
 end
